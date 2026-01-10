b1a701e95d54716ad7371915613600723755c651
johan-droid <sahooashutosh2022@gmail.com>
2026-01-11 01:19:42 +0530

    fixes

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,52 +8,52 @@ let autoSlideInterval;
 const mainCharactersData = [
     {
         "name": "Shinichi Kudo",
-        "image": "https://www.detectiveconanworld.com/wiki/images/3/3b/Shinichi_Kudo_Profile.jpg",
+        "image": null,
         "description": "Main character of the series, and Ran's love interest. He was shrunk into a child after being forced to take APTX 4869."
     },
     {
         "name": "Conan Edogawa",
-        "image": "https://www.detectiveconanworld.com/wiki/images/0/00/Conan_Edogawa_Profile.jpg",
+        "image": "assets/conan-mystery-hero.png",
         "description": "The child form of Shinichi Kudo. He chases the Black Organization to regain his original body while solving cases."
     },
     {
         "name": "Ran Mouri",
-        "image": "https://www.detectiveconanworld.com/wiki/images/9/93/Ran_Mouri_Profile.jpg",
+        "image": null,
         "description": "Shinichi's childhood friend and love interest. She takes care of Conan and her father, unaware of Conan's true identity."
     },
     {
         "name": "Kogoro Mouri",
-        "image": "https://www.detectiveconanworld.com/wiki/images/c/c5/Kogoro_Mouri_Profile.jpg",
+        "image": null,
         "description": "A private detective and Ran's father. Known as 'Sleeping Kogoro' due to Conan's help in solving cases."
     },
     {
         "name": "Ai Haibara",
-        "image": "https://www.detectiveconanworld.com/wiki/images/2/23/Ai_Haibara_Profile.jpg",
+        "image": null,
         "description": "The creator of APTX 4869 who shrunk herself to escape the Black Organization. She now helps Conan."
     },
     {
         "name": "Hiroshi Agasa",
-        "image": "https://www.detectiveconanworld.com/wiki/images/d/d7/Hiroshi_Agasa_Profile.jpg",
+        "image": null,
         "description": "An eccentric inventor and the first person to learn Conan's true identity. He provides Conan's gadgets."
     },
     {
         "name": "Heiji Hattori",
-        "image": "https://www.detectiveconanworld.com/wiki/images/7/75/Heiji_Hattori_Profile.jpg",
+        "image": null,
         "description": "The 'Great Detective of the West' from Osaka. He is Shinichi's rival and one of his closest friends."
     },
     {
         "name": "Shuichi Akai",
-        "image": "https://www.detectiveconanworld.com/wiki/images/a/a2/Shuichi_Akai_Profile.jpg",
+        "image": null,
         "description": "An FBI agent who infiltrated the Black Organization. He is a master sniper and a key ally to Conan."
     },
     {
         "name": "Kaitou Kid",
-        "image": "https://www.detectiveconanworld.com/wiki/images/0/07/Kaitou_Kid_Profile.jpg",
+        "image": null,
         "description": "A phantom thief who uses magic tricks and disguises. He is a rival to Conan but often helps him."
     },
     {
         "name": "Juzo Megure",
-        "image": "https://www.detectiveconanworld.com/wiki/images/1/1a/Juzo_Megure_Profile.jpg",
+        "image": null,
         "description": "A dedicated police inspector from the Tokyo Metropolitan Police District who trusts Shinichi implicitly."
     }
 ];
@@ -61,27 +61,27 @@ const mainCharactersData = [
 const blackOrgCharactersData = [
     {
         "name": "Gin",
-        "image": "https://www.detectiveconanworld.com/wiki/images/7/77/Gin_Profile.jpg",
+        "image": "assets/gin-villain.png",
         "description": "A high-ranking executive member. He forced Shinichi to take the poison that shrunk him."
     },
     {
         "name": "Vodka",
-        "image": "https://www.detectiveconanworld.com/wiki/images/c/c5/Vodka_Profile.jpg",
+        "image": null,
         "description": "Gin's secretary and partner. He is loyal but less intelligent than his counterpart."
     },
     {
         "name": "Vermouth",
-        "image": "https://www.detectiveconanworld.com/wiki/images/4/4c/Vermouth_Profile.jpg",
+        "image": null,
         "description": "A master of disguise and a favorite of the Boss. She knows Conan's true identity but keeps it secret."
     },
     {
         "name": "Bourbon",
-        "image": "https://www.detectiveconanworld.com/wiki/images/e/e8/Rei_Furuya_Profile.jpg",
+        "image": null,
         "description": "An undercover agent working within the organization. He is also known as Amuro Tooru."
     },
     {
         "name": "Kir",
-        "image": "https://www.detectiveconanworld.com/wiki/images/8/85/Hidemi_Hondou_Profile.jpg",
+        "image": null,
         "description": "An undercover CIA agent posing as a TV reporter. She relays information to the FBI."
     }
 ];
